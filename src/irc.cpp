36355fb36d954968cf81b2931d76b7e106e76dce
mvs <reamils@gmail.com>
2014-01-06 09:39:20 -0800

    changes

--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #foocoin2TEST3\r");
-            Send(hSocket, "WHO #foocoin2TEST3\r");
+            Send(hSocket, "JOIN #primocoin2TEST3\r");
+            Send(hSocket, "WHO #primocoin2TEST3\r");
         } else {
-            // randomly join #foocoin00-#foocoin99
+            // randomly join #primocoin00-#primocoin99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #foocoin2%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #foocoin2%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #primocoin2%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #primocoin2%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
---
+++ src/obj/build.h
@@ -0,0 +1,2 @@
+// No build information available
+#define BUILD_DATE "2014-01-05 16:16:25 -0800"
